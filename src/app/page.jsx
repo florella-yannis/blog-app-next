0930db418229f87a932e32e1cdae8a0246034ef0
florella-yannis <florellayannis@gmail.com>
2024-02-22 15:53:52 +0100

    init + layout + component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 import styles from "./homepage.module.css";
 
+
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+
+  </div>;
 }
---
+++ src/components/cardList/CardList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./cardList.module.css"
+
+const CardList = () => {
+    return (
+        <div className={styles.container}>
+            cardList
+        </div>
+    );
+};
+
+export default CardList;
