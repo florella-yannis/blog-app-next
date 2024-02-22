daf92ced50863d794ff2f0b375dee82ba6580d69
florella-yannis <florellayannis@gmail.com>
2024-02-22 16:00:10 +0100

    home page structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,19 @@
+import Featured from "@/components/featured/Featured";
 import styles from "./homepage.module.css";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
 
 
 export default function Home() {
   return <div>
-
+<div className={styles.container}>
+  <Featured />
+  <CategoryList />
+  <div className={styles.content}>
+  <CardList />
+  <Menu />
+  </div>
+</div>
   </div>;
 }
