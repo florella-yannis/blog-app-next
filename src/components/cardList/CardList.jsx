38fedcd3ee59dcca81b012d14eb4ffff995880b6
florella-yannis <florellayannis@gmail.com>
2024-02-26 19:57:43 +0100

    blog post card component

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import styles from "./cardList.module.css"
-import Pagination from '../pagination/Pagination';
+import React from "react";
+import styles from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
+import Image from "next/image";
+import Card from "../card/Card";
 
 const CardList = () => {
-    return (
-        <div className={styles.container}>
-            cardList
-            <Pagination />
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <Pagination />
+    </div>
+  );
 };
 
 export default CardList;
