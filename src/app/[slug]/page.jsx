10dfe3b09f17624ee54439eb62701b032ae0591b
florella-yannis <florellayannis@gmail.com>
2024-02-27 22:31:57 +0100

    create Page and Module

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./singlePage.module.css";
+
+const SinglePage = () => {
+  return <div className={styles.container}>single page</div>;
+};
+
+export default SinglePage;
