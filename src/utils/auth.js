565d2185d087f2676464690f7ef8f2171f3e52b5
florella-yannis <florellayannis@gmail.com>
2024-03-05 23:09:12 +0100

    init
    npm install @prisma/client @auth/prisma-adapter
    npm add prisma --save-dev
    npx prisma init --datasource-provider mongodb
    npx prisma generate

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { Prisma } from "@prisma/client"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
     providers: [
+        PrismaAdapter(Prisma),
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
