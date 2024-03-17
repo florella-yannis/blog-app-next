e97277ae0f14467866657134258b8c741bbd2958
florella-yannis <florellayannis@gmail.com>
2024-03-17 20:11:19 +0100

    last update

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -13,21 +13,3 @@ export const authOptions = {
         }),
     ],
 }
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { Prisma } from "@prisma/client"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-
-export const authOptions = {
-    providers: [
-        PrismaAdapter(Prisma),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
-        }),
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-    ],
-}
